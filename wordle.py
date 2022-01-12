e1773ca6aa8739bcbd9be9486170bdcf0d8b03a2
Andrew Heine <drewheine@gmail.com>
2022-01-12 14:24:31 -0500

    cleanup

--- wordle.py
+++ wordle.py
@@ -2,14 +2,35 @@ import sys
 from allWords import sortedEnglishWords as englishWords
 
 class GameState(object):
-    def __init__(self, answer):
-        self.knownCorrect = [None] * 5  # green letters
+    def __init__(self, englishWords, answer):
+        self.knownCorrect = [None] * 5     # green letters
         self.minLetterInstances = dict()   # yellow letters
         self.maxLetterInstances = dict()   # accumulation of greys
-        self.answer = answer            # optional
+        self.answer = answer               # optional known answer
+        self.words = englishWords
+
+    def guess(self):
+        return self.words[0]
+
+    def done(self):
+        return self.knownCorrect.count(None) == 0
+
+    def printState(self):
+        print("Greens: " + str(self.knownCorrect))
+        print("Min letter counts: " + str(self.minLetterInstances))
+        print("Max letter counts: " + str(self.maxLetterInstances))
+        print("Num candidates remaining: " + str(len(self.words)))
+
+        if (len(self.words) < 5):
+            print(self.words)
+
+        print("\n\n")
+
+    def filterWords(self):
+        self.words = [word for word in self.words if self.wordAllowed(word)]
 
     def wordAllowed(self, word):
-        """ Check if a word is allowed based on the game state """
+        """ Determine if a word fits what we know about the Wordle solution """
 
         # All green letters must be present
         for idx, letter in enumerate(self.knownCorrect):
@@ -29,6 +50,10 @@ class GameState(object):
         return True
 
     def promptForResult(self):
+        """
+        Ask the user what colors were returned by the Wordle web app.
+        Not used if we know the answer already (see autoUpdateState())
+        """
         print("Input result:")
         colors = input()
 
@@ -42,7 +67,7 @@ class GameState(object):
             else:
                 self.maxLetterInstances[guess[idx]] = self.minLetterInstances.get(guess[idx]) or 0
 
-    def update(self, guess):
+    def autoUpdateState(self, guess):
         """ Only used when we already know the answer and we're just demonstrating the program. """
         self.minLetterInstances = {}
         tempAnswer = self.answer
@@ -61,42 +86,36 @@ class GameState(object):
             else:
                 self.maxLetterInstances[letter] = self.minLetterInstances.get(letter) or 0
 
+#
 # Two modes.
 # If answer is provided in args, this runs automatically.
 # Otherwise, we don't know the answer, and we need to ask the user for knownCorrect, yellows, etc.
+#
+
 answer = None
 if (len(sys.argv) > 1):
     answer = sys.argv[1]
 else:
-    print("You'll have to enter your own results from the wordle app. Use G, B, and Y.")
+    print("You'll have to enter the results from the wordle app. Use G, B, and Y.")
 
-gameState = GameState(answer)
+gameState = GameState(englishWords, answer)
 attempts = 0
 
 while attempts < 20:
-    guess = englishWords[0]
-    print("Attempt " + str(attempts + 1))
+    attempts += 1
+    guess = gameState.guess()
+    print("Attempt " + str(attempts))
     print("Guess: " + guess)
     
     if answer:
-        gameState.update(guess)
+        gameState.autoUpdateState(guess)
     else:
         gameState.promptForResult()
 
-    if gameState.knownCorrect.count(None) == 0:
+    if gameState.done():
         print("Done!")
         break
 
-    englishWords = [word for word in englishWords if gameState.wordAllowed(word)]
-    attempts += 1
-
-    print("Greens: " + str(gameState.knownCorrect))
-    print("Min letter counts: " + str(gameState.minLetterInstances))
-    print("Max letter counts: " + str(gameState.maxLetterInstances))
-    print("Num candidates remaining: " + str(len(englishWords)))
-
-    if (len(englishWords) < 5):
-        print(englishWords)
-
-    print("\n\n")
+    gameState.filterWords()
+    gameState.printState()
 
