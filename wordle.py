b70291947119f40315cdd33d2dc13996cca352ec
Andrew Heine <drewheine@gmail.com>
2022-01-12 14:44:28 -0500

    UI

--- wordle.py
+++ wordle.py
@@ -3,7 +3,7 @@ from allWords import sortedEnglishWords as englishWords
 
 class GameState(object):
     def __init__(self, englishWords, answer):
-        self.knownCorrect = [None] * 5     # green letters
+        self.knownCorrect = ['*'] * 5     # green letters
         self.minLetterCounts = dict()   # yellow letters
         self.exactLetterCounts = dict()   # accumulation of greys
         self.answer = answer               # optional known answer
@@ -13,16 +13,17 @@ class GameState(object):
         return self.words[0]
 
     def done(self):
-        return self.knownCorrect.count(None) == 0
+        return self.knownCorrect.count('*') == 0
 
     def printState(self):
-        print("Greens: " + str(self.knownCorrect))
-        print("Min letter counts: " + str(self.minLetterCounts))
-        print("Exact letter counts: " + str(self.exactLetterCounts))
-        print("Num candidates remaining: " + str(len(self.words)))
+        print("".join(self.knownCorrect).upper())
+        print("Letters present: " + str(self.minLetterCounts))
+        print("Known counts: " + str(self.exactLetterCounts))
         if (len(self.words) < 5):
-            print(self.words)
-        print("\n\n")
+            plural = ' candidates' if len(self.words) > 1 else ' candidate'
+            print(str(len(self.words)) + plural + " remaining: " + ', '.join(self.words))
+        else:
+            print(str(len(self.words)) + " candidates remaining")
 
     def filterWords(self):
         self.words = [word for word in self.words if self.wordAllowed(word)]
@@ -32,7 +33,7 @@ class GameState(object):
 
         # All green letters must be present
         for idx, letter in enumerate(self.knownCorrect):
-            if letter and word[idx] != letter:
+            if letter != "*" and word[idx] != letter:
                 return False
 
         # All yellow letters must be present (and we might need more than 1)
@@ -68,14 +69,11 @@ class GameState(object):
     def autoUpdateState(self, guess):
         """ Only used when we already know the answer and we're just demonstrating the program. """
         self.minLetterCounts = {}
-        tempAnswer = self.answer
         for idx, letter in enumerate(guess):
-            if tempAnswer[idx] == letter:
+            if self.answer[idx] == letter:
                 self.knownCorrect[idx] = letter
-                tempAnswer = tempAnswer.replace(letter, '*', 1)
-                self.minLetterCounts.setdefault(letter, 0)
-                self.minLetterCounts[letter] += 1
 
+        tempAnswer = self.answer
         for letter in guess:
             if tempAnswer.find(letter) >= 0:
                 tempAnswer = tempAnswer.replace(letter, '', 1)
@@ -100,10 +98,11 @@ gameState = GameState(englishWords, answer)
 attempts = 0
 
 while attempts < 20:
+    print("\n\n")
     attempts += 1
     guess = gameState.guess()
     print("Attempt " + str(attempts))
-    print("Guess: " + guess)
+    print(guess.upper())
     
     if answer:
         gameState.autoUpdateState(guess)
@@ -111,7 +110,7 @@ while attempts < 20:
         gameState.promptForResult()
 
     if gameState.done():
-        print("Done!")
+        print("\nDone!\n")
         break
 
     gameState.filterWords()
